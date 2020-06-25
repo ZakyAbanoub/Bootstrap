dcaae68ef89da1afb43042c91734502329b26759
AbanoubZaky <zaky.abanoub@gmail.com>
2020-06-25 08:49:35 +0200

    Add all the project in one page
    finish all the project in one page and make it responsive.

--- js/plugins.js
+++ js/plugins.js
@@ -4,11 +4,18 @@ $(function(){
     var navH      = $('.navbar').innerHeight();
 
     $('.slider .image-me').height( winH - (upperH + navH));
-});
 
-// $(window).resize(function () {
-//     var winH   = $(window).height(),
-//     upperH = $(".upper-bar").innerHeight(),
-//     navH   = $(".navbar").innerHeight();
-//     $(".slide, .carousel-item").height(winH - (upperH + navH));
-//    });
+    $('.featured-work ul li').on('click', function (){
+        $(this).addClass('active').siblings().removeClass('active');
+        console.log($(this).data('class'));
+        if ($(this).data('class') === 'all') {
+            $('.featured-work .row img').css('opacity', 1);
+        }else{
+            $('.featured-work .row img').css('opacity', 0.2);
+            $($(this).data('class')).css('opacity', 1);
+        }
+
+    });
+
+});
+$('.number').counterUp({time: 2000});
